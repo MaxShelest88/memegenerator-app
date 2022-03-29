b71b0bf69fa19ef1d9cc99f8cecb8b70f0309a61
Maksim Shelest <shelest.ks@gmai.com>
2022-03-30 00:08:40 +0300

    creted state for images

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,25 +1,55 @@
-import React from 'react';
+import React, {useState} from 'react';
 import MyInput from "./UI/input/MyInput";
 import MyButton from "./UI/button/MyButton";
 import "../styles/Meme.css"
+import memesData from "../memedata"
+
+
 
 const Meme = (props) => {
 
     const btnClick = (e) => {
         e.preventDefault()
-        console.log('click')
+        getRandomMemeUrl()
+    }
+
+    const [meme, setMeme] = React.useState({
+        topText:"",
+        bottomText:"",
+        randomImage:"http://i.imgflip.com/1bij.jpg"
+    })
+
+    const [allMemeImages, setAllMemeImages] = React.useState(memesData.data.memes)
+
+
+    function getRandomNumber(maxNum){
+        return Math.floor(Math.random() * maxNum)
+    }
+
+    function getRandomMemeUrl(){
+        let index = getRandomNumber(allMemeImages.length)
+        setMeme(prevMeme=>({
+            ...prevMeme,
+            randomImage: allMemeImages[index].url
+        }))
     }
 
     return (
-        <form className="meme">
-            <div className="meme__container _container">
-                <div className="meme__inputs">
-                    <MyInput  placeholder='Текст наверху'/>
-                    <MyInput placeholder='Текст внизу'/>
+        <div>
+            <form className="meme">
+                <div className="meme__container _container">
+                    <div className="meme__inputs">
+                        <MyInput  placeholder='Текст наверху'/>
+                        <MyInput placeholder='Текст внизу'/>
+                    </div>
+                    <MyButton onClick={btnClick}>Get a new meme image  🖼</MyButton>
                 </div>
-                <MyButton onClick={btnClick}>Get a new meme image  🖼</MyButton>
+            </form>
+            <div className="image _container">
+                <img src={meme.randomImage} />
             </div>
-        </form>
+        </div>
+
     );
 };
 
